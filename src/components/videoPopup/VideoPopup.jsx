010a85de2440bc38a07c561498a2cfe97599319f
Sushajkhan <sushajkhan@gmail.com>
2023-12-01 12:10:09 +0530

    video popup fixed

--- src/components/videoPopup/VideoPopup.jsx
+++ src/components/videoPopup/VideoPopup.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactPlayer from "react-player/youtube";
+import { VscChromeClose } from "react-icons/vsc";
 
-
+import "./style.scss";
 
 const VideoPopup = ({ show, setShow, videoId, setVideoId }) => {
     const hidePopup = () => {
@@ -13,7 +14,7 @@ const VideoPopup = ({ show, setShow, videoId, setVideoId }) => {
             <div className="opacityLayer" onClick={hidePopup}></div>
             <div className="videoPlayer">
                 <span className="closeBtn" onClick={hidePopup}>
-                    Close
+                <VscChromeClose /> 
                 </span>
                 <ReactPlayer
                     url={`https://www.youtube.com/watch?v=${videoId}`}
